4dce2b2eefab979303523b2d676703797d4f7f86
Joshua Carroll <joshcarroll21@gmail.com>
2019-08-13 21:39:41 -0500

    Finished implementing home and away will tags for CSS support

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 			$roster = fopen("BRoster.csv","r"); // What's the filename? You didn't upload it
 		}
 		
-		while(! feof($roster)) {
+		while(!feof($roster)) {
 			$row = fgetcsv($roster);
 			if ($row[0] == $jerseyNumber) {
 				$name = $row[1];
@@ -18,8 +18,14 @@
 		}
 		
 		fclose($roster);
-
-		$names = $_POST["names"] . $jerseyNumber . " " . $name;
+		
+		$playerDiv = "<div class='player'><span class='jerseyNumber'>" . $jerseyNumber . "</span> <span class='name'>" . $name . "</span><span class='position'>" . $position . "</span></div>"
+		
+		if ($lastCharacter == "+") {
+			$ourTeam = $_POST["hdnOurTeam"] . $playerDiv;
+		} elseif ($lastCharacter == "-") {
+			$theirTeam = $_POST["hdnTheirTeam"] . $playerDiv;
+		}
 	}
 ?>
 
@@ -40,6 +46,8 @@
 					else if(keynum == 13) { //if enter key
 						document.getElementById("ourTeam").innerHTML = "";
 						document.getElementById("theirTeam").innerHTML = "";
+						document.getElementById("hdnOurTeam").value = "";
+						document.getElementById("hdnTheirTeam").value = "";
 						return false;
 					}
 					else {
@@ -51,8 +59,8 @@
     <body>
     	<form name="form1" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 			<input type="text" name="jerseyNumber" id="jerseyNumber" onkeyup="num_keypress(num)" autofocus size="1">
-			<input type="hidden" name="ourTeam" value="<?= $ourTeam ?>">
-			<input type="hidden" name="theirTeam" value="<?= $theirTeam ?>">
+			<input type="hidden" id="hdnOurTeam" name="ourTeam" value="<?= $ourTeam ?>">
+			<input type="hidden" id="hdnTheirTeam" name="theirTeam" value="<?= $theirTeam ?>">
 			<div id="ourTeam">
 				<?= $ourTeam ?>
 			</div>
