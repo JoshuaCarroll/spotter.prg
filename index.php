90a8bfbede8489a99bb96b70c9c2f4a4c5acd49c
George Carroll <ghcarrolljr@gmail.com>
2019-08-21 19:12:57 -0500

    Trying to get 13 as a return

--- index.php
+++ index.php
@@ -45,7 +45,13 @@
 			function num_keypress(event) {
 				var char = event.value;
                 var lastChar = char[char.length -1];
-                var key = event.keyCode;
+                if(lastChar && lastChar.which){ //if which property of event object is supported (NN4)
+					key = lastChar.which; //character code is contained in NN4's which property
+				}
+				else{
+					key = lastChar.keyCode; //character code is contained in IE's keyCode property
+				}
+                //var key = event.keyCode;
                 alert(key);
 				//if ((40 <= key) && (key <= 36)){} // 48 = $ 57 = (
 				if ((lastChar == "+") || (lastChar == "-")) { // 43 = + 45 = -
