7b35f15c7e5951ee6c1c6ccf07e65b1763bdfa31
George Carroll <ghcarrolljr@gmail.com>
2019-08-22 15:52:24 -0500

    Update index.php

--- index.php
+++ index.php
@@ -91,7 +91,7 @@
     </head>
     <body>
     	<form name="form1" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-			<input type="text" name="jerseyNumber" id="jerseyNumber" onkeypress="num_keypress(event)"><!--return num_keypress(event) -->
+			<input type="text" name="jerseyNumber" id="jerseyNumber" ><!--return num_keypress(event) -->
 			<input type="hidden" id="hdnOurTeam" name="hdnOurTeam" value="<?= $ourTeam ?>">
 			<input type="hidden" id="hdnTheirTeam" name="hdnTheirTeam" value="<?= $theirTeam ?>">
 			<div id="ourTeam"><?= $ourTeam ?></div>
