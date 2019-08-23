96d62ed975fb7c9e177428832e69d73f3f434107
George Carroll <ghcarrolljr@gmail.com>
2019-08-23 16:55:20 -0500

    Update index.php

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
                     stop;
                 }
 				else if (lastChar == "$") { // $ send keycode 122
-                    element.body.requestFullscreen(); // document.body.requestFullscreen();
+                    element.requestFullscreen(); // document.body.requestFullscreen();
 					document.getElementById("jerseyNumber").value = "";          
                     //event.preventDefault();
 				}
