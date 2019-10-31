dd42c94f1329be25787b479516bf1921f903b4aa
George Carroll <ghcarrolljr@gmail.com>
2019-10-31 15:33:41 -0500

    Test

--- insert.php
+++ insert.php
@@ -2,7 +2,21 @@
 $con = mysqli_connect("localhost","root","21802Ghc<","BRoster");
 
 if ($con)
-{print_r($_FILES['csvfile']);}
+{
+    $file = $_FILES['csvfile']['tmp_name'];
+    $handle = fopen($file,"r");
+    while(($cont=fgetcsv($handle,1000,","))!==false)
+    {
+        $num=$con[0];
+        $name=$con[1];
+        $pos=$con[2];
+        $class=$con[3];
+        $h=$co[4];
+        $w=$con[5];
+        $query="CREATATE TABLE $table($num INT(3),$name VARCHAR(30),$pos VARCHAR(5),$class VARCHAR(2),$h VARCHAR(5),$w INT(3))";
+        echo $query,"<br>";
+    }
+}
 else
 {echo "connection failed";}
 ?>
