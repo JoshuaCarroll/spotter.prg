76c1b7728757021bde6ab8076f33b4d82d2e4009
George Carroll <ghcarrolljr@gmail.com>
2019-10-31 15:50:20 -0500

    Final test

--- insert.php
+++ insert.php
@@ -13,9 +13,9 @@ if ($con)
         $class=$cont[3];
         $h=$cont[4];
         $w=$cont[5];
-//        $query="CREATATE TABLE $table($num INT(3),$name VARCHAR(30),$pos VARCHAR(5),$class VARCHAR(2),$h VARCHAR(5),$w INT(3));";
-//        echo $query,"<br>";
-        echo "it works";
+        $query="CREATATE TABLE $table($num INT(3),$name VARCHAR(30),$pos VARCHAR(5),$class VARCHAR(2),$h VARCHAR(5),$w INT(3));";
+        echo $query,"<br>";
+        
     }
 }
 else
