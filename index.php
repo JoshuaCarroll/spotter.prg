de55007ab7830249b7d858ddc9ef1bac8ce82f48
George Carroll <ghcarrolljr@gmail.com>
2019-10-31 12:04:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 </head>
 <body>
     <form action="insert.php" method="post" enctype="multipart/form-data">
-    <input type="file" name="csvfile" required="required" /> //changed
+    <input type="file" name="csvfile" required="required" /> 
     <input type="submit" valua="upload" />
     </form>
     <p>This is a test</p>
