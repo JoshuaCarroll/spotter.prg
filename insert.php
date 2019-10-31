4e40c8d02c40d620e6c85ab4aa144e78533f6b5d
George Carroll <ghcarrolljr@gmail.com>
2019-10-31 12:51:41 -0500

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,11 +2,7 @@
 $con = mysqli_connect("localhost","root","21802Ghc<","BRoster");
 
 if ($con)
-{
-    echo "connected";
-    //print_r($_FILES['csvfile']);
-
-}
+{print_r($_FILES['csvfile']);}
 else
 {echo "connection failed";}
 ?>
