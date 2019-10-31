1acb14cbaf7f315d6fe1416cd0d3a203c64b8a29
George Carroll <ghcarrolljr@gmail.com>
2019-10-31 15:43:34 -0500

    Update insert.php

--- insert.php
+++ insert.php
@@ -5,8 +5,8 @@ if ($con)
 {
     $file = $_FILES['csvfile']['tmp_name'];
     $handle = fopen($file,"r");
-//    while(($cont=fgetcsv($handle,1000,","))!==false)
-//    {
+    while(($cont=fgetcsv($handle,1000,","))!==false)
+    {
 //        $num=$con[0];
 //        $name=$con[1];
 //        $pos=$con[2];
@@ -15,7 +15,8 @@ if ($con)
 //        $w=$con[5];
 //        $query="CREATATE TABLE $table($num INT(3),$name VARCHAR(30),$pos VARCHAR(5),$class VARCHAR(2),$h VARCHAR(5),$w INT(3));";
 //        echo $query,"<br>";
-//    }
+        echo "it works";
+    }
 }
 else
 {echo "connection failed";}
