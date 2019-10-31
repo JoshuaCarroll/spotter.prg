0925b563b92a6af01bdb90d5fee47bc9f77cf066
George Carroll <ghcarrolljr@gmail.com>
2019-10-30 21:34:56 -0500

    removed db name

--- page2.php
+++ page2.php
@@ -6,7 +6,7 @@ $password = "21802Ghc<";
 $message = "Connected";
 // Create connection
 
-$conn = mysqli_connect("localhost", "root", "21802Ghc<", "player");
+$conn = mysqli_connect("localhost", "root", "21802Ghc<");
 
 // Check connection
 
