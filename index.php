d4d9114b8f45674548dc914e839efcff8809e9e8
George Carroll <ghcarrolljr@gmail.com>
2019-10-31 09:05:27 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html >
+<head>
+    <title>csv insert</title>
+</head>
+<body>
+    <form action="insert.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="BRoster.csv" required="required" />
+    <input type="submit" valua="upload" />
+    </form>
+</body>
+</html>
