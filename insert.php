7803aa2fe54833eb8d7231b108b5abd17249a84a
George Carroll <ghcarrolljr@gmail.com>
2019-10-31 12:20:46 -0500

    Update insert.php

--- insert.php
+++ insert.php
@@ -3,7 +3,9 @@ $con=mysql_connect("localhost","root","21802Ghc<","BRoster");
 
 if ($con)
 {
-$file=$_FILES('BRoster')('tmp_name')
+print_r($_FILES['csvfile']);
 
 }
+else
+{echo "connection failed";}
 ?>
