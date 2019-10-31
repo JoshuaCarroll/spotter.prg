cdf94ef797cd98a43680b71c3e9a38cd2cd0651d
George Carroll <ghcarrolljr@gmail.com>
2019-10-31 15:49:03 -0500

    had $con not $cont

--- insert.php
+++ insert.php
@@ -7,12 +7,12 @@ if ($con)
     $handle = fopen($file,"r");
     while(($cont=fgetcsv($handle,1000,","))!==false)
     {
-        $num=$con[0];
-        $name=$con[1];
-        $pos=$con[2];
-        $class=$con[3];
-        $h=$con[4];
-        $w=$con[5];
+        $num=$cont[0];
+        $name=$cont[1];
+        $pos=$cont[2];
+        $class=$cont[3];
+        $h=$cont[4];
+        $w=$cont[5];
 //        $query="CREATATE TABLE $table($num INT(3),$name VARCHAR(30),$pos VARCHAR(5),$class VARCHAR(2),$h VARCHAR(5),$w INT(3));";
 //        echo $query,"<br>";
         echo "it works";
