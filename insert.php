cbe153b06e09d17a486e744c90e135a1e171b92c
George Carroll <ghcarrolljr@gmail.com>
2019-10-31 21:03:10 -0500

    Final test

--- insert.php
+++ insert.php
@@ -5,17 +5,28 @@ if ($con)
 {
     $file = $_FILES['csvfile']['tmp_name'];
     $handle = fopen($file,"r");
+    $i=0;
     while(($cont=fgetcsv($handle,1000,","))!==false)
     {
-        $num=$cont[0];
-        $name=$cont[1];
-        $pos=$cont[2];
-        $class=$cont[3];
-        $h=$cont[4];
-        $w=$cont[5];
-        $query="CREATATE TABLE $table($num INT(3),$name VARCHAR(30),$pos VARCHAR(5),$class VARCHAR(2),$h VARCHAR(5),$w INT(3));";
-        echo $query,"<br>";
-        
+        $table=rtrim($_FILES['csvfile']['name'],".csv");
+        if(i==0){
+            $num=$cont[0];
+            $name=$cont[1];
+            $pos=$cont[2];
+            $class=$cont[3];
+            $h=$cont[4];
+            $w=$cont[5];
+            $query="CREATATE TABLE $table($num INT(3),$name VARCHAR(30),$pos VARCHAR(5),$class VARCHAR(2),$h VARCHAR(5),$w INT(3));";
+            echo $query,"<br>";
+            mysqli_query($con,$query);
+
+        }
+        else
+        {
+            $query="INSERT INTO $table ($num,$name,$pos,$class,$h,$w) VALUES ('$cont[0]','$cont[1]','$cont[2]','$cont[3]','$cont[4]','$cont[5]');";
+            echo $query,"<br>";
+        }
+        $i++;
     }
 }
 else
