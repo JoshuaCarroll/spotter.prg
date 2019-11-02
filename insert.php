7362de32ba3cf876672a9b4d9fad623c77890856
George Carroll <ghcarrolljr@gmail.com>
2019-11-02 16:05:04 -0500

    changed db and size

--- insert.php
+++ insert.php
@@ -1,51 +1,38 @@
 <?php
-$con = mysqli_connect("localhost","root","21802Ghc<","BRoster");
+$con = mysqli_connect("localhost","root","21802Ghc<", 'SpotterDB');
+// Open db SpotterDB
 if($con === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 else
 {
+    
     $file = "BRoster.csv";
     $handle = fopen($file,"r");
- //   $i=0;
- //   $table=rtrim($_FILES['csvfile']['name'],".csv");
-    echo $table;
-    $query="CREATE TABLE players(Number INT(3),Name VARCHAR(30),Position VARCHAR(5),Class VARCHAR(2),Height VARCHAR(5),Weight INT(4));";
-    if ((mysqli_query($con,$query))!==false)
+
+    $query="CREATE TABLE BRoster(Number INT(3),Name VARCHAR(30),Position VARCHAR(5));"; 
+    // removed ,Class VARCHAR(2),Height VARCHAR(5),Weight INT(4)
+    
+    if ((mysqli_query($con,$query))!==false) //test for table
     {
-    echo "table created";    
+        echo "table created";    
     }
     else
-    {echo "table exists";}
-//    echo $query,"<br>";
+    {
+        echo "table exists";
+        
+    
+    }
     while(($cont = fgetcsv($handle,1000,","))!==false)
     {
-
-//        if(i==0){
-//            $num=$cont[0];
-//            $name=$cont[1];
-//            $pos=$cont[2];
-//            $class=$cont[3];
-//            $h=$cont[4];
-//            $w=$cont[5];
-            
- //           mysqli_query($con,$query);
-
-//        }
-//        else
-//        {
-            $query="INSERT INTO players(Number,Name,Position,Class,Height,Weight) VALUES('$cont[0]','$cont[1]','$cont[2]','$cont[3]','skip','0');";
+            $query="INSERT INTO BRoster(Number,Name,Position) VALUES('$cont[0]','$cont[1]','$cont[2]');";
             
             if((mysqli_query($con,$query))!==false){
                 echo "Records inserted successfully";
                 } else{
                 echo "ERROR: Not able to execute" . mysqli_error($con) . "<br>";
                 }
-//            echo $query,"<br>";
             }
- //       $i++;
- //   }
-    
 }
 
 echo "Finished";
