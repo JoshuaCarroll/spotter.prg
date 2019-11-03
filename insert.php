c78d1a622da613eae4eede5729bf2586d8a2a636
George Carroll <ghcarrolljr@gmail.com>
2019-11-02 20:57:56 -0500

    Added code for ORoster db

--- insert.php
+++ insert.php
@@ -9,9 +9,8 @@ else
     
     $file = "BRoster.csv";
     $handle = fopen($file,"r");
-
+    // Create a table for the Bruins
     $query="CREATE TABLE BRoster(Number INT(3),Name VARCHAR(30),Position VARCHAR(5), INDEX (Number));"; 
-    // removed ,Class VARCHAR(2),Height VARCHAR(5),Weight INT(4)
     
     if ((mysqli_query($con,$query))!==false) //test for table
     {
@@ -20,19 +19,40 @@ else
     else
     {
         echo "table exists <br>";
-        
-    
     }
     while(($cont = fgetcsv($handle,1000,","))!==false)
     {
-            $query="INSERT INTO BRoster(Number,Name,Position) VALUES('$cont[0]','$cont[1]','$cont[2]');";
-            
-            if((mysqli_query($con,$query))!==false){
+        $query="INSERT INTO BRoster(Number,Name,Position) VALUES('$cont[0]','$cont[1]','$cont[2]');";    
+            if((mysqli_query($con,$query))!==false)
+            {
                 echo "Records inserted successfully <br>";
-                } else{
+            } else{
                 echo "ERROR: Not able to execute" . mysqli_error($con) . "<br>";
-                }
             }
+    }
+    //Open ORoster.csv
+    $file = "ORoster.csv";
+    $handle = fopen($file,"r");
+    // Create a table for the Bruins
+    $query="CREATE TABLE BRoster(Number INT(3),Name VARCHAR(30),Position VARCHAR(5), INDEX (Number));"; 
+    if ((mysqli_query($con,$query))!==false) //test for table
+    {
+        echo "table created <br>";    
+    }
+    else
+    {
+        echo "table exists <br>";
+    }
+    while(($cont = fgetcsv($handle,1000,","))!==false)
+    {
+        $query="INSERT INTO ORoster(Number,Name,Position) VALUES('$cont[0]','$cont[1]','$cont[2]');";    
+            if((mysqli_query($con,$query))!==false)
+            {
+                echo "Records inserted successfully <br>";
+            } else{
+                echo "ERROR: Not able to execute" . mysqli_error($con) . "<br>";
+            }
+    }
 }
 
 echo "Finished";
