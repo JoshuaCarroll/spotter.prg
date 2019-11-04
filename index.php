3d38bc22715a36120cd91508de93950d12a0fc45
George Carroll <ghcarrolljr@gmail.com>
2019-11-04 12:34:07 -0600

    Update index.php

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ if($conn === false){
         }
         
     
-        $query = "INSERT INTO Screen (Ourteam) VALUE ('$arr');";
+        $query = "INSERT INTO Screen (Ourteam) VALUE (1,'$arr[0]');";
         if((mysqli_query($conn,$query))==false)
         {
             echo "ERROR" . mysqli_error($conn) . "<br>";
