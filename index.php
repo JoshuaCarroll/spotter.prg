94e600b94e9857a3102c93d81ad92e51eb0859bd
George Carroll <ghcarrolljr@gmail.com>
2019-11-03 20:32:35 -0600

    Update index.php

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ if($con === false){
         }
         $query = "SELECT (Ourteam) FROM Screen;";
         $result = mysqli_query($con,$query);
-        $Screen = mysql_fetch_row($result);
+        $Screen = $ourTeam;//mysql_fetch_row($result);
  //       echo $Screen;
 	}
 mysqli_close($conn);
