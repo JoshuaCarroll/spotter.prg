dacf560dbafc3fdda2199447aa39d2b4e3a740bd
George Carroll <ghcarrolljr@gmail.com>
2019-11-03 18:22:37 -0600

    Create AddScreen.php

---
+++ AddScreen.php
@@ -0,0 +1,12 @@
+<html >
+<head>
+    <title>Creat Screen db</title>
+</head>
+<body>
+    <form action="insert2.php" method="post" enctype="multipart/form-data">
+<!--    <input type="file" name="csvfile" required="required" />--> 
+    <input type="submit" valua="upload" />
+    </form>
+    
+</body>
+</html>
