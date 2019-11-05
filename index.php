a682690a3cb42f50f09af691d1ec32b4c3665856
George Carroll <ghcarrolljr@gmail.com>
2019-11-04 19:31:08 -0600

    Update index.php

--- index.php
+++ index.php
@@ -49,13 +49,10 @@ if($conn === false){
         }
         
         
-        echo $ourTeam;
-        echo $theirTeam;
-        
         $query = "INSERT INTO `Screen` (`Ourteam`, `Theirteam`) VALUES ('$ourTeam', NULL) ;";
         if((mysqli_query($conn,$query))==false)
         {
-            //echo "ERROR" . mysqli_error($conn) . "<br>";
+            echo "ERROR" . mysqli_error($conn) . "<br>";
         }
         
         $query = "SELECT * FROM Screen;";
