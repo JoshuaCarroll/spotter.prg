12961732744bbf98f1b335a9afcf5fcb01cb49d5
George Carroll <ghcarrolljr@gmail.com>
2019-11-06 08:22:28 -0600

    Update index2.php

--- index2.php
+++ index2.php
@@ -41,7 +41,7 @@ else {
 			}
 // There is something in $playerDiv that causes and error
 			$playerDiv = "<div class='player'><span class='jerseyNumber'>" . $jerseyNumber . "</span> <span class='name'>" . $name . "</span> <span class='position'>" . $position . "</span></div>";
-            $hold = mysql_real_escape_string($playerDiv);
+            $hold = "this is a test";
 			$query = "INSERT INTO `Screen` (`player`, `team`) VALUES ('$hold', '$lastCharacter') ;";
         
 			if((mysqli_query($conn,$query))==false)
