9338957964223cd32968fc47c154828a425e7618
George Carroll <ghcarrolljr@gmail.com>
2019-11-06 09:58:38 -0600

    Update index2.php

--- index2.php
+++ index2.php
@@ -43,10 +43,10 @@ else {
 			$playerDiv = "&lt;div class&#61\'player\'>&lt;span class=\'jerseyNumber\'>" . $jerseyNumber . "&lt;/span> &lt;span class=\'name\'>" . $name . "&lt;/span> &lt;span class=\'position\'>" . $position . "&lt;/span>&lt;/div>";
             
 			$query = "INSERT INTO `Screen` (`player`, `team`) VALUES ('$playerDiv', '$lastCharacter') ;";
-            echo $query;
+            
 			if((mysqli_query($conn,$query))==false)
 			{
-				//echo "ERROR" . mysqli_error($conn) . "<br>";
+				echo "ERROR" . mysqli_error($conn) . "<br>";
 			}
             else {
                 $query = "SELECT * FROM Screen;";
