760a685aa728cad556081617e7840ec791e28984
George Carroll <ghcarrolljr@gmail.com>
2019-11-08 09:38:25 -0600

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ if (!empty($_POST)) { // Checks to see if it received a form submission
         echo $query;
 		$results = mysqli_query($conn,$query);
 		// create an array of the data for player $jerseyNumber
-        if ($results){
+        if ($results == true){
 		$row = mysqli_fetch_array($results,MYSQLI_NUM);
 		//Read the name and position
 		$name = $row[1];
