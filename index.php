c1c4ac07d99570da08ce9656e4cad722d654d8da
Joshua Carroll <JoshuaCarroll@users.noreply.github.com>
2019-11-07 19:47:50 -0600

    Changed the checkbox

--- index.php
+++ index.php
@@ -5,69 +5,65 @@ $refreshInterval = "checked";
 // Create connection
 
 $conn = mysqli_connect("localhost", "root", "21802Ghc<", "SpotterDB");
-if($conn === false) {
-    echo "<script type='text/javascript'>alert('Didn't connect');</script>";
-}
-else {
+
+if (!empty($_POST)) { // Checks to see if it received a form submission
 	$refreshInterval = ""; // Clear this because this person is entering values
-	
-	if (!empty($_POST)) { // Checks to see if it received a form submission
-		if (($_POST["jerseyNumber"]) == "clear") {
-			mysqli_query($conn,"Delete * from Screen");
-		}
-		else {
-			$jerseyNumber = substr_replace($_POST["jerseyNumber"],"",-1);
-			$lastCharacter = substr($_POST["jerseyNumber"], -1);
-		
-        	if ($lastCharacter == "+") { // Test for Bruin
-				// call the data for $jerseyNumber
-				$query = "SELECT * FROM BRoster WHERE Number=$jerseyNumber;";
-				$results = mysqli_query($conn,$query);
-				// create an array of the data for player $jerseyNumber
-				$row=mysqli_fetch_array($results,MYSQLI_NUM);
-				//Read the name and position
-				$name = $row[1];
-				$position = $row[2];
-			}
-        	if ($lastCharacter == "-") { // test for Opposition
-				// call the data for $jerseyNumber
-				$query = "SELECT * FROM ORoster WHERE Number=$jerseyNumber;";
-				$results = mysqli_query($conn,$query);
-				// create an array of the data for player $jerseyNumber
-				$row=mysqli_fetch_array($results,MYSQLI_NUM);
-				//Read the name and position
-				$name = $row[1];
-				$position = $row[2];
-			}
 
-			$playerDiv = "<div class=\'player\'><span class=\'jerseyNumber\'>" . $jerseyNumber . "</span> <span class=\'name\'>" . $name . "</span> <span class=\'position\'>" . $position . "</span></div>";
-            $query = "INSERT INTO `Screen` (`player`, `team`) VALUES ('$playerDiv', '$lastCharacter') ;";
-
-            if((mysqli_query($conn,$query))==false)
-            {
-                echo "ERROR" . mysqli_error($conn) . "<br>";
-            }
-			
-        }
-    } // no... THIS.. is the end of POST
-	
-	// Initialize the variables
-    $ourTeam = "";
-	$theirTeam = "";
-	
-	$query = "SELECT * FROM Screen;";
-	$result = mysqli_query($conn,$query);    
-	while ($row = mysqli_fetch_assoc($result)) {
-		if ($row['team']=="+") {
-			$ourTeam = $ourTeam . $row['player'];  // Add the lastest to the END, not the beginning
-		}
-		elseif ($row['team']=="-") {
-			$theirTeam = $theirTeam . $row['player'];
-		}
+	if (($_POST["jerseyNumber"]) == "clear") {
+		mysqli_query($conn,"Delete * from Screen");
 	}
+	else {
+		$jerseyNumber = substr_replace($_POST["jerseyNumber"],"",-1);
+		$lastCharacter = substr($_POST["jerseyNumber"], -1);
 
-	mysqli_close($conn);
+	if ($lastCharacter == "+") { // Test for Bruin
+			// call the data for $jerseyNumber
+			$query = "SELECT * FROM BRoster WHERE Number=$jerseyNumber;";
+			$results = mysqli_query($conn,$query);
+			// create an array of the data for player $jerseyNumber
+			$row=mysqli_fetch_array($results,MYSQLI_NUM);
+			//Read the name and position
+			$name = $row[1];
+			$position = $row[2];
+		}
+	if ($lastCharacter == "-") { // test for Opposition
+			// call the data for $jerseyNumber
+			$query = "SELECT * FROM ORoster WHERE Number=$jerseyNumber;";
+			$results = mysqli_query($conn,$query);
+			// create an array of the data for player $jerseyNumber
+			$row=mysqli_fetch_array($results,MYSQLI_NUM);
+			//Read the name and position
+			$name = $row[1];
+			$position = $row[2];
+		}
+
+		$playerDiv = "<div class=\'player\'><span class=\'jerseyNumber\'>" . $jerseyNumber . "</span> <span class=\'name\'>" . $name . "</span> <span class=\'position\'>" . $position . "</span></div>";
+    $query = "INSERT INTO `Screen` (`player`, `team`) VALUES ('$playerDiv', '$lastCharacter') ;";
+
+    if((mysqli_query($conn,$query))==false)
+    {
+	echo "ERROR" . mysqli_error($conn) . "<br>";
+    }
+
 }
+} // no... THIS.. is the end of POST
+
+// Initialize the variables
+$ourTeam = "";
+$theirTeam = "";
+
+$query = "SELECT * FROM Screen;";
+$result = mysqli_query($conn,$query);    
+while ($row = mysqli_fetch_assoc($result)) {
+	if ($row['team']=="+") {
+		$ourTeam = $ourTeam . $row['player'];  // Add the lastest to the END, not the beginning
+	}
+	elseif ($row['team']=="-") {
+		$theirTeam = $theirTeam . $row['player'];
+	}
+}
+
+mysqli_close($conn);
 ?>
 
 <!DOCTYPE html>
