5ff2617f265f27a35cc4d51426a04d19a747dda6
George Carroll <ghcarrolljr@gmail.com>
2019-11-08 09:33:52 -0600

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ if (!empty($_POST)) { // Checks to see if it received a form submission
 			$playerTable = "ORoster";
 		}
 		
-		$query = "SELECT player,team FROM BRoster WHERE Number=$jerseyNumber;";
+		$query = "SELECT * FROM BRoster WHERE Number=$jerseyNumber;";
         echo $query;
 		$results = mysqli_query($conn,$query);
 		// create an array of the data for player $jerseyNumber
