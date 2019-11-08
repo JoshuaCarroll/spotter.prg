42f806faf3b7ff743ab1ed885012ef82458a5611
George Carroll <ghcarrolljr@gmail.com>
2019-11-08 13:06:31 -0600

    Cant preset ck

--- index.php
+++ index.php
@@ -95,7 +95,7 @@ mysqli_close($conn);
 			<div id="ourTeam"><?= $ourTeam ?></div>
 			<div id="theirTeam"><?= $theirTeam ?></div>
 			<label for="refresh">Auto refresh</label>
-            <input type="checkbox" name="refresh" id="refresh" checked />
+            <input type="checkbox" name="refresh" id="refresh" />
         </form>
         
 		<script type="text/javascript">
