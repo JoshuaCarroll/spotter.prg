ac790bc90693db3cd664f49efc58560c221ef306
George Carroll <ghcarrolljr@gmail.com>
2019-11-08 09:42:05 -0600

    testing row

--- index.php
+++ index.php
@@ -28,15 +28,13 @@ if (!empty($_POST)) { // Checks to see if it received a form submission
         echo $query;
 		$results = mysqli_query($conn,$query);
 		// create an array of the data for player $jerseyNumber
-        if ($results == true){
+        
 		$row = mysqli_fetch_array($results,MYSQLI_NUM);
 		//Read the name and position
+        echo $row;    
 		$name = $row[1];
         echo $name;
-		$position = $row[2];}
-        else{
-            echo ("ERROR" . mysqli_error($conn));
-        }
+		$position = $row[2];
 
 		$playerDiv = '<div class="player"><span class="jerseyNumber">' . $jerseyNumber . '</span> <span class="name">' . $name . '</span> <span class="position">' . $position . '</span></div>';
 		$query = "INSERT INTO Screen (player, team) VALUES ('" . $playerDiv . "', '" . $lastCharacter . "') ;";
