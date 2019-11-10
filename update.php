583ee547905acc40246c83d6b3e6cc55d78d5d8b
George Carroll <ghcarrolljr@gmail.com>
2019-11-10 14:34:12 -0600

    add error handeler

--- update.php
+++ update.php
@@ -19,7 +19,8 @@ if (!empty($_POST["url"])) {
 	echo $output;
 	$conn = mysqli_connect("localhost", "root", "Passw0rd", "SpotterDB");
 	mysqli_query($conn, "DELETE FROM $table;");
-	mysqli_query($conn, $output); // Execute the insert statement
+    if (mysqli_query($conn, $output) == false) {echo (mysqli_error_list($conn));}
+	 // Execute the insert statement
 	mysqli_close($conn);
 }
 
