0de10294c5c527af0beb1e4420ec16c5a0ef4e88
George Carroll <ghcarrolljr@gmail.com>
2019-11-24 11:07:29 -0600

    Update index.php

--- index.php
+++ index.php
@@ -6,15 +6,15 @@ $refreshInterval = "checked";
 
 $row = 1;
 if (($handle = fopen("../.sec/hold", "r")) !== FALSE) {
-//    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
 //        $num = count($data);
 //        echo "<p> $num fields in line $row: <br /></p>\n";
 //        $row++;
 //        for ($c=0; $c < $num; $c++) {
 //            echo $data[$c] . "<br />\n";
-    echo "read file";
+echo $data;
         } 
-//    } 
+    } 
     
 //} 
 else {echo "ERROR";}
