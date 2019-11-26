a628ecb1f622091da16c3f33b9ac558d3f8c21e1
Joshua Carroll <joshcarroll21@gmail.com>
2019-11-26 15:24:53 -0600

    Added included config file

--- update.php
+++ update.php
@@ -26,8 +26,9 @@
 		$url = "https://wt-90a32ef8ce619cb1158f5b6850fb1f12-0.sandbox.auth0-extend.com/maxprepsTeamScraper?url=" . $_POST["url"]; // path to the JSON file
 		$data = file_get_contents($url);  // load contents of the page 
 		$roster = json_decode($data, true); // parse the JSON into an object
+		
+		// Build insert statement
 		$output = "INSERT INTO $table (Number, Name, Position) VALUES ";
-
 		foreach ($roster as $player) {
 			if ($player[jersey]){
 				$output = $output . "('" . $player[jersey] . "','" . $player[name] . "','" . $player[position] . "'), ";
@@ -36,8 +37,12 @@
 		}
 		$output = $output . "(null,null,null);";
 
-		fclose($handle);
-
+		// Load config values
+		include 'config.php';
+		if ($host_name == "") {
+			die("<h3>You need to make a copy of the 'config.example.php' and name it 'config.php', then edit that file with the appropriate database access parameters.</h3>");
+		}
+		
 		$conn = mysqli_connect($host_name, $user_name, $password, $database);
 		if (mysqli_connect_errno()) {
 		  die('<p>Failed to connect to MySQL: '.mysqli_connect_error().'</p>');
